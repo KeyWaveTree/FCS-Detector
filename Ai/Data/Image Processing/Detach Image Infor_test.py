9c71ea7ef1c6d6c281d1a14b170c109f61c66e82
KeyWaveTree <damrok0815@gmail.com>
2023-05-14 22:32:09 +0900

    iamge tag

--- Ai/Data/Image Processing/Detach Image Infor_test.py
+++ Ai/Data/Image Processing/Detach Image Infor_test.py
@@ -8,12 +8,38 @@ import pandas as pd
 import cv2 as cv
 import os
 
+data_path = '../Clothing_Data'
+
+file_dict = {}
 #파일 순차적으로 리스트 뽑기
-def img_path_list(origin_path : str)->list:
-      all_cloth_folder = os.listdir(origin_path)
-      return all_cloth_folder
+def img_path_list(origin_path : str)->dict:
+      for (root, directories, files) in os.walk(origin_path):
+            print('디렉토리')
+            for d in directories:
+                  d_path = os.path.join(root, d)
+                  print(d_path)
+
+            print('파일들')
+            for file in files:
+                  file_path = os.path.join(d, file)
+                  print(file, d)
+                  if d not in file_dict:
+                        file_dict[d]=[]
+                  file_dict[d].append(file_path)
 
 
-print(img_path_list('../Clothing_Data/zalando'))
+# #path에서
+# data_path = os.path.join('../Clothing_Data')
+# #디렉토리도 루트를 지정해야 한다.
+# #루트가 있어야 아레의 내용을 확인 할 수 있는데 지금 경로만 하여 밑에 있는 애들을 못찾은 것이다.
+#
+# #파일 순차적으로 리스트 뽑기
+# def img_path_list(origin_path : str)->dict:
+#       for (root, directories, files) in os.walk(origin_path):
+#             for name in files:
+#                   print(os.path.join(root, name))
+#             print()
+
+img_path_list(data_path)
 
 #파일 순서 목록을
