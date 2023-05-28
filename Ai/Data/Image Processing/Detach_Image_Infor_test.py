1cd1188a784a0e0f742259f57514ba889b13841b
KeyWaveTree <damrok0815@gmail.com>
2023-05-28 23:07:51 +0900

    test update

--- Ai/Data/Image Processing/Detach_Image_Infor_test.py
+++ Ai/Data/Image Processing/Detach_Image_Infor_test.py
@@ -3,46 +3,47 @@
 # 데이터 다운로드(데이터 불러오기)
 # 사람 랜드 마크 이용한 얼굴 블러처리 후 저장
 
+
 import mediapipe as mp
 import pandas as pd
 import numpy as np
-import cv2 as cv
+import cv2
 import os
 
 from PIL import Image, ImageDraw
 
-data_path = os.path.join('../Clothing_Data')
-# #디렉토리도 루트를 지정해야 한다.
-# #루트가 있어야 아레의 내용을 확인 할 수 있는데 지금 경로만 하여 밑에 있는 애들을 못찾은 것이다.
-
-file_dict = {}
-#파일 순차적으로 리스트 뽑기
-def img_path_list1(origin_path : str)->dict:
-      for (root, directories, files) in os.walk(origin_path):
-            print('디렉토리')
-            for d in directories:
-                  d_path = os.path.join(root, d)
-                  print(d_path)
-
-            print('파일들')
-            for file in files:
-                  file_path = os.path.join(d, file)
-                  print(file, d)
-                  if d not in file_dict:
-                        file_dict[d]=[]
-                  file_dict[d].append(file_path)
-
-
-# #path에서
+# data_path = os.path.join('../Clothing_Data')
+# # #디렉토리도 루트를 지정해야 한다.
+# # #루트가 있어야 아레의 내용을 확인 할 수 있는데 지금 경로만 하여 밑에 있는 애들을 못찾은 것이다.
+#
+# file_dict = {}
 # #파일 순차적으로 리스트 뽑기
-def img_path_list2(origin_path : str)->dict:
-      for (root, directories, files) in os.walk(origin_path):
-            for name in files:
-                  print(os.path.join(root, name))
-            print()
-
-img_path_list1(data_path)
-img_path_list2(data_path)
+# def img_path_list1(origin_path : str)->dict:
+#       for (root, directories, files) in os.walk(origin_path):
+#             print('디렉토리')
+#             for d in directories:
+#                   d_path = os.path.join(root, d)
+#                   print(d_path)
+#
+#             print('파일들')
+#             for file in files:
+#                   file_path = os.path.join(d, file)
+#                   print(file, d)
+#                   if d not in file_dict:
+#                         file_dict[d]=[]
+#                   file_dict[d].append(file_path)
+#
+#
+# # #path에서
+# # #파일 순차적으로 리스트 뽑기
+# def img_path_list2(origin_path : str)->dict:
+#       for (root, directories, files) in os.walk(origin_path):
+#             for name in files:
+#                   print(os.path.join(root, name))
+#             print()
+#
+# img_path_list1(data_path)
+# img_path_list2(data_path)
 
 #파일 순서 목록을
 #먼저 사진 하나로 실험
@@ -58,11 +59,15 @@ if not os.path.exists('../ImgProcess'):
       os.mkdir('../ImgProcess')
 
 if not os.path.exists('../ImgProcess/Clothing_Landmark'):
-      os.mkdir('../ImgaeProcess/Clothing_Landmark')
+      os.mkdir('../ImgProcess/Clothing_Landmark')
 
 with img_pose.Pose(
       static_image_mode=True,
-      model_complexity=2,
       enable_segmentation=True,
       min_detection_confidence=0.5) as pose:
 
+      #이미지를 파일을 rgb로 먼저 변환 및 저장
+      origin_img=Image.open(test_path)
+      img_array=np.array(origin_img)
+      #print(img_array.shape) - height X width X 3
+      # print(img_array) 0- 1- 2-
