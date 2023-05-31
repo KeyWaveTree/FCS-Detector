e2d1622cff189cc1bd48959dc5e9fa6cfb0a82ac
KeyWaveTree <damrok0815@gmail.com>
2023-05-31 23:31:50 +0900

    contour

--- Ai/Data/Image Processing/Detach_Image_Infor_test.py
+++ Ai/Data/Image Processing/Detach_Image_Infor_test.py
@@ -2,11 +2,10 @@
 # 리펙토링 단계
 # 데이터 다운로드(데이터 불러오기)
 # 사람 랜드 마크 이용한 얼굴 블러처리 후 저장
-import cv2.version
 import mediapipe as mp
 import pandas as pd
 import numpy as np
-import cv2 as cv
+import cv2
 import os
 
 from PIL import Image, ImageDraw
@@ -53,23 +52,72 @@ img_pose = mp.solutions.pose #미디어 파이프 솔루션
 
 
 test_path='../Clothing_Data/zalando/shirt/0DB22O00B-Q11@8.jpg'
-#경로 세팅
+
+key_path = test_path.split('/')[3] #특정 옷 태그 뽑기
+key_name = '1'
+
+# 경로 세팅
 if not os.path.exists('../ImgProcess'):
       os.mkdir('../ImgProcess')
 
 if not os.path.exists('../ImgProcess/Clothing_Landmark'):
       os.mkdir('../ImgProcess/Clothing_Landmark')
 
+if not os.path.exists('../ImgProcess/Clothing_Landmark/' + key_path):
+      os.mkdir('../ImgProcess/Clothing_Landmark/' + key_path)
+
+# 처리한 이미지 저장
+landmark_img_path = '../ImgProcess/Clothing_Landmark/' + key_path + '/' + key_name
+# 이미지 색상 저장 csv file로 변환
+#이미지 사람과 옷 태두리
 with img_pose.Pose(
-      static_image_mode=True,
-      enable_segmentation=True,
-      min_detection_confidence=0.5) as pose:
+static_image_mode=True,
+enable_segmentation=True,
+min_detection_confidence=0.5) as pose:
 
-      #이미지를 파일을 rgb로 먼저 변환 및 저장
-      origin_img=Image.open(test_path)
-      img_array=np.array(origin_img)
-      #print(img_array.shape) - height X width X 3
-      # print(img_array) 0- 1- 2-
-
+      #근데 이미지 확장성이 jpg라서 용량이 큼 용량을 줄일 필요가 있지 않을까?
       #if of openCV
-      img=cv2.imread()
+      #이미지 처리를 할때 우선적으로 관심영역만 표시를 할까?
+      #아니면 원본에서 처리를 한다음에 인식을 해야 할까?
+      origin_img=cv2.imread(test_path) #이미지 읽어오기
+      nagative_color_img=cv2.bitwise_not(origin_img)
+      nagative_img =cv2.cvtColor(nagative_color_img, cv2.COLOR_BGR2GRAY) # 그레이 색상으로 하는 이유는 노이즈 제거와 속도 때문에
+      positive_img=cv2.cvtColor(origin_img, cv2.COLOR_BGR2GRAY)# 그레이 색상으로 하는 이유는 노이즈 제거와 속도 때문에
+
+      _, nagative_img_binary = cv2.threshold(nagative_img, 150, 255, cv2.THRESH_BINARY)
+      _, positive_img_binary = cv2.threshold(positive_img, 150, 255, cv2.THRESH_BINARY)
+
+      nagative_find, nagative_hier = cv2.findContours(nagative_img_binary,
+                                                      mode=cv2.RETR_EXTERNAL, method=cv2.CHAIN_APPROX_NONE)
+      positive_find, positive_hier = cv2.findContours(positive_img_binary,
+                                                      mode=cv2.RETR_EXTERNAL, method=cv2.CHAIN_APPROX_NONE)
+      #케니 필터를 이용해서 윤곽선 추출 -보류
+      index=0
+      while index>=0:
+            #사람
+            #nagavite
+
+            nagative_img_contour=cv2.drawContours(origin_img,
+                                         contours=find, contourIdx=index,
+                                         color=(0, 0, 255), lineType=cv2.LINE_8, hierarchy=hier)
+            #positive
+
+
+            positive_img_contour = cv2.drawContours(origin_img,
+                                           contours=find, contourIdx=index,
+                                           color=(0, 255, 0), lineType=cv2.LINE_8, hierarchy=hier)
+
+            img_contour=cv2.bitwise_and(nagative_img_contour, positive_img_contour)
+      # roi = origin_img[y:y + h, x:x + w]
+      #
+      # # 옷
+      # thres1 = np.random.randint(0, 50)
+      # thres2 = np.random.randint(thres1, 100)
+      # edge=cv2.Canny(roi, threshold1=thres1, threshold2=thres2)
+      # 이미지 처리 후 처리용 폴더 경로에(새로운) 처리한 이미지 저장해야 함
+
+      cv2.imshow(landmark_img_path, img_contour)  # 절대 경로, 이미지 읽을 때 옵션
+      cv2.imwrite(landmark_img_path+'.jpg', img_contour)
+
+      cv2.waitKey(0)
+      cv2.destroyAllWindows()
