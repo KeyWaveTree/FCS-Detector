00baf98028daed68d0aa7428923c9176e28372f5
KeyWaveTree <damrok0815@gmail.com>
2023-06-09 14:21:52 +0900

    img_processing finish FCS_ImgProcess

--- Ai/Data/Image Processing/Detach_Image_Infor.py
+++ Ai/Data/Image Processing/Detach_Image_Infor.py
@@ -1,145 +1,151 @@
-#완성파일 
- 
-#사람이 같이 포함된 이미지 불러오기
-# 리펙토링 단계
-# 데이터 다운로드(데이터 불러오기)
-# 사람 랜드 마크 이용한 얼굴 블러처리 후 저장
-
-import mediapipe as mp
-import pandas as pd
-import numpy as np
-import cv2
-import os
-
-# #디렉토리도 루트를 지정해야 한다.
-# #루트가 있어야 아레의 내용을 확인 할 수 있는데 지금 경로만 하여 밑에 있는 애들을 못찾은 것이다.
-Image_Data_Path = os.path.join('../Clothing_Data')
-
-class FCS_ImageProcess:
-      def __init__(self, data_path : str):
-            self.data_path = data_path
-
-#폴더 안 이미지 파일 순차적으로 딕셔너리 얻기
-def img_path_map(origin_path : str)->dict:
-      file_dict = {}
-      # 디렉토리를 재귀로 탐색에서 파일만 추출하는 반복문
-      for (root, directories, files) in os.walk(origin_path): #각 루트, 디렉토리, 파일들
-            #print('파일들')
-            for file in files:
-                  file_path = os.path.join(root, file)
-                  #print(file, root)
-                  if root not in file_dict:
-                        file_dict[root]=[]
-                  file_dict[root].append(file_path)
-
-      return file_dict
-
-mp_img_pose = mp.solutions.pose #미디어 파이프 솔루션
-mp_pose_img_drawing=mp.solutions.drawing_utils
-
-test_path='../Clothing_Data/zalando/shirt/0DB22O00B-Q11@8.jpg'
-
-key_path = test_path.split('/')[3] #특정 옷 태그 뽑기
-key_name = '1'
-
-# 경로 세팅
-if not os.path.exists('../ImgProcess'):
-      os.mkdir('../ImgProcess')
-
-if not os.path.exists('../ImgProcess/Clothing_Landmark'):
-      os.mkdir('../ImgProcess/Clothing_Landmark')
-
-if not os.path.exists('../ImgProcess/Clothing_Landmark/' + key_path):
-      os.mkdir('../ImgProcess/Clothing_Landmark/' + key_path)
-
-# 처리한 이미지 저장
-img_path = '../ImgProcess/Clothing_Landmark/' + key_path + '/' + key_name
-
-#pass
-def img_contour(get_img_path: str)->cv2: #path
-
-      #근데 이미지 확장성이 jpg라서 용량이 큼 용량을 줄일 필요가 있지 않을까?
-      #if of openCV
-      #이미지 처리를 할때 우선적으로 관심영역만 표시를 할까?
-      #아니면 원본에서 처리를 한다음에 인식을 해야 할까?
-      origin_img=cv2.imread(get_img_path,cv2.COLOR_BGR2HSV) #이미지 읽어오기
-      #자른다음 하자 #노이즈제거는 GRAY이지만
-
-
-      gaussian_blur =cv2.GaussianBlur(origin_img ,ksize=(3,3),sigmaX=0)
-
-      thresh_img= np.median(gaussian_blur)
-      thresh_lower=int(max(0,(1.0 - 0.55) *thresh_img))
-      thresh_upper=int(max(255, (1.0 + 0.55)*thresh_img))
-
-      #케니필터로
-      cv2.adaptiveThreshold(gaussian_blur, 255, cv2.ADAPTIVE_THRESH_MEAN_C , cv2.THRESH_BINARY_INV, 9 ,10)
-      edge=cv2.Canny(gaussian_blur, threshold1=thresh_lower, threshold2=thresh_upper)
-
-      #binary_img=cv2.bitwise_or(nagative_binary_img, positive_binary_img, edge)
-      #
-      # _, nagative_img_binary = cv2.threshold(nagative_img, 150, 255, cv2.THRESH_BINARY)
-      # _, positive_img_binary = cv2.threshold(positive_img, 150, 255, cv2.THRESH_BINARY)
-      #
-      # nagative_find, nagative_hier = cv2.findContours(nagative_img_binary,
-      #                                                 mode=cv2.RETR_EXTERNAL, method=cv2.CHAIN_APPROX_NONE)
-      # positive_find, positive_hier = cv2.findContours(positive_img_binary,
-      #                                                 mode=cv2.RETR_EXTERNAL, method=cv2.CHAIN_APPROX_NONE)
-      #
-      # index=0
-      # while index>=0:
-      #       #사람
-      #       #nagavite
-      #
-      #       nagative_img_contour=cv2.drawContours(origin_img,
-      #                                    contours=nagative_find, contourIdx=index,
-      #                                    color=(0, 0, 255), lineType=cv2.LINE_8, hierarchy=nagative_hier)
-      #       #positive
-      #
-      #
-      #       positive_img_contour = cv2.drawContours(origin_img,
-      #                                      contours=positive_find, contourIdx=index,
-      #                                      color=(0, 255, 0), lineType=cv2.LINE_8, hierarchy=positive_hier)
-      #
-      #       img_contour=cv2.bitwise_and(nagative_img_contour, positive_img_contour)
-
-      return edge
-
-def people_img_landmark(get_origin_img:cv2)->cv2:
-      with mp_img_pose.Pose(
-      static_image_mode=True,
-      enable_segmentation=True,
-      min_detection_confidence=0.5) as pose:
-
-            test_img = cv2.cvtColor(get_origin_img, cv2.COLOR_BGR2RGB)
-
-            img_pose = pose.process(test_img)
-
-            landmarks = img_pose.pose_landmarks.landmark #각 랜드마크의 활성화 상태 리스트
-            left_shoulder =[landmarks[mp_img_pose.PoseLandmark.LEFT_SHOULDER.value].x, #왼쪽어깨 랜드마크 x좌표
-                            landmarks[mp_img_pose.PoseLandmark.LEFT_SHOULDER.value].y] #왼쪽어깨 랜드마크 y좌표
-
-            right_shoulder = [landmarks[mp_img_pose.PoseLandmark.RIGHT_SHOULDER.value].x,
-                              landmarks[mp_img_pose.PoseLandmark.RIGHT_SHOULDER.value].y ]
-            print(left_shoulder, right_shoulder)
-
-            reset_test_img = cv2.cvtColor(test_img, cv2.cv2.COLOR_RGB2BGR)
-
-            mp_pose_img_drawing.draw_landmarks(reset_test_img, img_pose.pose_landmarks, mp_img_pose.POSE_CONNECTIONS,
-                                    mp_pose_img_drawing.DrawingSpec(color=(245, 117, 66), thickness=2,circle_radius=2),
-                                    mp_pose_img_drawing.DrawingSpec(color=(245, 66, 230),thickness=2, circle_radius=2))
-
-      return reset_test_img
-#지금 모든 경로는 안씀
-path=img_path_map(Image_Data_Path)
-
-origin_image= cv2.imread(test_path)
-
-people_landmark = people_img_landmark(origin_image)
-cv2.imshow(img_path, people_landmark)
-cv2.imwrite(img_path + '.jpg', people_landmark)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
-#이미지 사람 태두리 and 옷 패턴 윤곽처리
-# 찍은 사진을 분석
+# #완성파일
+#
+# #사람이 같이 포함된 이미지 불러오기
+# # 리펙토링 단계
+# # 데이터 다운로드(데이터 불러오기)
+# # 사람 랜드 마크 이용한 얼굴 블러처리 후 저장
+#
+# import mediapipe as mp
+# import pandas as pd
+# import numpy as np
+# import cv2
+# import os
+#
+# # #디렉토리도 루트를 지정해야 한다.
+# # #루트가 있어야 아레의 내용을 확인 할 수 있는데 지금 경로만 하여 밑에 있는 애들을 못찾은 것이다.
+# Image_Data_Path = os.path.join('../Clothing_Data')
+#
+# class FCS_ImageProcess:
+#       def __init__(self, data_path : str):
+#             self.data_path = data_path
+#
+# #폴더 안 이미지 파일 순차적으로 딕셔너리 얻기
+# def image_path_map(origin_path : str)->dict:
+#       file_dict = {}
+#       # 디렉토리를 재귀로 탐색에서 파일만 추출하는 반복문
+#       for (root, directories, files) in os.walk(origin_path): #각 루트, 디렉토리, 파일들
+#             #print('파일들')
+#             for file in files:
+#                   file_path = os.path.join(root, file)
+#                   #print(file, root)
+#                   if root not in file_dict:
+#                         file_dict[root]=[]
+#                   file_dict[root].append(file_path)
+#
+#       return file_dict
+#
+#
+# def landmark_img_position(origin_img_path)-> dict or int:
+#     with mp_img_pose.Pose(
+#     static_image_mode=True,
+#     enable_segmentation=True,
+#     min_detection_confidence=0.5) as pose:
+#         copy_img=cv2.imread(origin_img_path)
+#         try:
+#             color_copy_img = cv2.cvtColor(copy_img, cv2.COLOR_RGB2BGR)
+#             img_pose = pose.process(color_copy_img)
+#             landmarks = img_pose.pose_landmarks.landmark  #각 랜드마크의 활성화 상태 리스트
+#
+#             origin_img_height, origin_img_width, _ = color_copy_img.shape
+#             img_shape=[origin_img_width, origin_img_height]
+#
+#             left_shoulder = (landmarks[mp_img_pose.PoseLandmark.LEFT_SHOULDER].x ,
+#             landmarks[mp_img_pose.PoseLandmark.LEFT_SHOULDER].y )
+#             right_shoulder=(landmarks[mp_img_pose.PoseLandmark.RIGHT_SHOULDER].x,
+#             landmarks[mp_img_pose.PoseLandmark.RIGHT_SHOULDER].y )
+#
+#             left_mouth=(landmarks[mp_img_pose.PoseLandmark.MOUTH_LEFT.value].x,
+#             landmarks[mp_img_pose.PoseLandmark.MOUTH_LEFT.value].y)
+#             right_mouth=(landmarks[mp_img_pose.PoseLandmark.MOUTH_RIGHT.value].x,
+#             landmarks[mp_img_pose.PoseLandmark.MOUTH_RIGHT.value].y)
+#
+#             left_pinky=(landmarks[mp_img_pose.PoseLandmark.LEFT_PINKY.value].x,
+#             landmarks[mp_img_pose.PoseLandmark.LEFT_PINKY.value].y)
+#
+#             shoulder = {'l':np.multiply(left_shoulder, img_shape).astype(int),
+#                         'r':np.multiply(right_shoulder,img_shape).astype(int)}
+#             mouth = {'l':np.multiply(left_mouth , img_shape).astype(int),
+#                      'r': np.multiply(right_mouth, img_shape).astype(int)}
+#
+#             right_elbow_x = np.multiply(landmarks[mp_img_pose.PoseLandmark.RIGHT_ELBOW].x, img_shape[0]).astype(int)
+#             left_pinky = np.multiply(left_pinky, img_shape).astype(int)
+#
+#             middle_neck_y= (((shoulder['l'][1]+shoulder['r'][1])//2)+((mouth['l'][1] + mouth['r'][1]) // 2))//2
+#
+#             return {'x1':right_elbow_x-5,'y1': middle_neck_y, 'x2':left_pinky[0], 'y2':left_pinky[1]}
+#
+#         except AttributeError:
+#             return -1
+#
+#         except cv2.error:
+#             return -1
+#
+#
+#
+# def canny_edge_img(roi_path):
+#     roi=cv2.imread(roi_path)
+#     glay_img = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+#     gaussian_blur =cv2.GaussianBlur(glay_img ,ksize=(3,3),sigmaX=0)
+#
+#     thresh_img= np.median(gaussian_blur)
+#     thresh_lower=int(max(0,(1.0 - 0.22) *thresh_img))
+#     thresh_upper=int(max(255, (1.0 + 0.22)*thresh_img))
+#
+#     #케니필터로
+#     cv2.adaptiveThreshold(gaussian_blur, 255, cv2.ADAPTIVE_THRESH_MEAN_C , cv2.THRESH_BINARY_INV, 9 ,10)
+#     edge=cv2.Canny(gaussian_blur, threshold1=thresh_lower, threshold2=thresh_upper)
+#
+#     return edge
+#
+# mp_img_pose = mp.solutions.pose  #미디어 파이프 솔루션
+# mp_pose_img_drawing=mp.solutions.drawing_utils
+#
+# img_path_map=image_path_map(Image_Data_Path)
+#
+#
+# for img_path_key in img_path_map:
+#       key_name = 0; edge_path = '../ImgProcess/Clothing_Egde/'; img_split_path = '../ImgProcess/Clothing_Wear/'
+#       if not os.path.exists('../ImgProcess'):os.mkdir('../ImgProcess')
+#
+#       if not os.path.exists(edge_path):os.mkdir(edge_path)
+#
+#       if not os.path.exists(img_split_path): os.mkdir(img_split_path)
+#
+#       for img_path in img_path_map[img_path_key]:
+#
+#             key_path = img_path.split('\\')[2]
+#             edge_write_path= edge_path + key_path +'/%s_%d.jpg' % (key_path,key_name)
+#             img_write_path= img_split_path + key_path +'/%s_%d.jpg' % (key_path,key_name)
+#
+#             origin_img=cv2.imread(img_path)
+#             # 경로 세팅
+#             if not os.path.exists(edge_path + key_path): os.mkdir(edge_path + key_path)
+#             if not os.path.exists( img_split_path + key_path ): os.mkdir(img_split_path + key_path)
+#
+#             if landmark_img_position(img_path)==-1 or os.path.exists(edge_write_path) or os.path.exists(img_write_path):
+#                 key_name += 1
+#                 continue
+#
+#             else:
+#               landmark_img = landmark_img_position(img_path)
+#               edge_img=canny_edge_img(img_path)
+#
+#               setting_origin_img=origin_img[landmark_img['y1']:landmark_img['y2'], landmark_img['x1']:landmark_img['x2']]
+#               setting_edge_img=edge_img[landmark_img['y1']:landmark_img['y2'], landmark_img['x1']:landmark_img['x2']]
+#               try:
+#                     cv2.imwrite(img_write_path, setting_origin_img)
+#                     cv2.imwrite(edge_write_path, setting_edge_img)
+#
+#               except cv2.error:
+#                     key_name += 1
+#                     continue
+#
+#             print(f'{key_path} 이미지 다운로드 중(' + str(key_name + 1) + '/' +
+#                   str(len(img_path_map[img_path_key])) + '): ' + str(key_name))
+#
+#             # 처리한 이미지 저장
+#             key_name += 1
+#
+# print('이미지 처리 완료')
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
--- Ai/Data/Image Processing/Detach_Image_Infor_test.py
+++ Ai/Data/Image Processing/Detach_Image_Infor_test.py
@@ -1,137 +1,89 @@
-#사람이 같이 포함된 이미지 불러오기
-# 리펙토링 단계
-# 데이터 다운로드(데이터 불러오기)
-# 사람 랜드 마크 이용한 얼굴 블러처리 후 저장
-import mediapipe as mp
-import pandas as pd
-import numpy as np
-import cv2
-import os
-
-data_path = os.path.join('../Clothing_Data')
-# #디렉토리도 루트를 지정해야 한다.
-# #루트가 있어야 아레의 내용을 확인 할 수 있는데 지금 경로만 하여 밑에 있는 애들을 못찾은 것이다.
-
-file_dict = {}
-#파일 순차적으로 리스트 뽑기
-def img_path_list1(origin_path : str)->dict:
-      for (root, directories, files) in os.walk(origin_path):
-            print('디렉토리')
-            for d in directories:
-                  d_path = os.path.join(root, d)
-                  print(d_path)
-
-            print('파일들')
-            for file in files:
-                  file_path = os.path.join(d, file)
-                  print(file, d)
-                  if d not in file_dict:
-                        file_dict[d]=[]
-                  file_dict[d].append(file_path)
-
-
-# #path에서
-# #파일 순차적으로 리스트 뽑기
-def img_path_list2(origin_path : str)->dict:
-      for (root, directories, files) in os.walk(origin_path):
-            for name in files:
-                  print(os.path.join(root, name))
-            print()
-
-img_path_list1(data_path)
-img_path_list2(data_path)
-
-#파일 순서 목록을
-#먼저 사진 하나로 실험
-
-#1 사진에서 랜드 마크 찍기 다른 폴더 사진으로 저장
-
-mp_img_pose = mp.solutions.pose #미디어 파이프 솔루션
-mp_pose_img_drawing=mp.solutions.drawing_utils
-
-test_path='../Clothing_Data/zalando/shirt/0DB22O00B-Q11@8.jpg'
-
-key_path = test_path.split('/')[3] #특정 옷 태그 뽑기
-key_name = '1'
-
-# 경로 세팅
-if not os.path.exists('../ImgProcess'):
-      os.mkdir('../ImgProcess')
-
-if not os.path.exists('../ImgProcess/Clothing_Landmark'):
-      os.mkdir('../ImgProcess/Clothing_Landmark')
-
-if not os.path.exists('../ImgProcess/Clothing_Landmark/' + key_path):
-      os.mkdir('../ImgProcess/Clothing_Landmark/' + key_path)
-
-# 처리한 이미지 저장
-img_path = '../ImgProcess/Clothing_Landmark/' + key_path + '/' + key_name
-
-#태두리 검출
-def img_contour(get_img_path: str)->cv2: #path
-
-      #근데 이미지 확장성이 jpg라서 용량이 큼 용량을 줄일 필요가 있지 않을까?
-      #if of openCV
-      #이미지 처리를 할때 우선적으로 관심영역만 표시를 할까?
-      #아니면 원본에서 처리를 한다음에 인식을 해야 할까?
-      origin_img=cv2.imread(get_img_path) #이미지 읽어오기
-
-      hsv_origin_img=cv2.cvtColor(origin_img,cv2.cv2.COLOR_BGR2HSV) #노이즈제거는 GRAY이지만
-
-      gaussian_blur =cv2.GaussianBlur(hsv_origin_img,ksize=(3,3),sigmaX=0)
-      cv2.threshold(gaussian_blur, 127, 255, cv2.THRESH_BINARY)
-      egde=cv2.Canny(gaussian_blur, threshold1=10, threshold2=250)
-
-      return egde
-      #cv2.imshow(landmark_img_path, egde)  # 절대 경로, 이미지 읽을 때 옵션
-      #cv2.imwrite(landmark_img_path + '.jpg', egde)
-
-
-# 이미지 사람 랜드마크
-
-#cv2.imshow(img_path, reset_test_img)
-#cv2.imwrite(img_path + '.jpg', reset_test_img)
-
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
-# nagative_color_img=cv2.bitwise_not(origin_img)
-# nagative_img =cv2.cvtColor(nagative_color_img, cv2.COLOR_BGR2GRAY) # 그레이 색상으로 하는 이유는 노이즈 제거와 속도 때문에
-# positive_img=cv2.cvtColor(origin_img, cv2.COLOR_BGR2GRAY)# 그레이 색상으로 하는 이유는 노이즈 제거와 속도 때문에
+# import mediapipe as mp
+# import numpy as np
+# import cv2
+# import os
 #
-# _, nagative_img_binary = cv2.threshold(nagative_img, 150, 255, cv2.THRESH_BINARY)
-# _, positive_img_binary = cv2.threshold(positive_img, 150, 255, cv2.THRESH_BINARY)
+# mp_img_pose = mp.solutions.pose #미디어 파이프 솔루션
+# mp_pose_img_drawing=mp.solutions.drawing_utils
 #
-# nagative_find, nagative_hier = cv2.findContours(nagative_img_binary,
-#                                                 mode=cv2.RETR_EXTERNAL, method=cv2.CHAIN_APPROX_NONE)
-# positive_find, positive_hier = cv2.findContours(positive_img_binary,
-#                                                 mode=cv2.RETR_EXTERNAL, method=cv2.CHAIN_APPROX_NONE)
+# test_path='../Clothing_Data/zalando/shirt/0DB22O00B-Q11@8.jpg'
 #
-# index=0
-# while index>=0:
-#       #사람
-#       #nagavite
+# key_path = test_path.split('/')[3] #특정 옷 태그 뽑기
+# key_name = '1'
 #
-#       nagative_img_contour=cv2.drawContours(origin_img,
-#                                    contours=nagative_find, contourIdx=index,
-#                                    color=(0, 0, 255), lineType=cv2.LINE_8, hierarchy=nagative_hier)
-#       #positive
+# # 경로 세팅
+# if not os.path.exists('../ImgProcess'):
+#       os.mkdir('../ImgProcess')
 #
+# if not os.path.exists('../ImgProcess/Clothing_Landmark'):
+#       os.mkdir('../ImgProcess/Clothing_Landmark')
 #
-#       positive_img_contour = cv2.drawContours(origin_img,
-#                                      contours=positive_find, contourIdx=index,
-#                                      color=(0, 255, 0), lineType=cv2.LINE_8, hierarchy=positive_hier)
+# if not os.path.exists('../ImgProcess/Clothing_Landmark/' + key_path):
+#       os.mkdir('../ImgProcess/Clothing_Landmark/' + key_path)
 #
-#       img_contour=cv2.bitwise_and(nagative_img_contour, positive_img_contour)
-# roi = origin_img[y:y + h, x:x + w]
+# # 처리한 이미지 저장
+# img_path = '../ImgProcess/Clothing_Landmark/' + key_path + '/'
 #
-# # 옷
-# thres1 = np.random.randint(0, 50)
-# thres2 = np.random.randint(thres1, 100)
-# edge=cv2.Canny(roi, threshold1=thres1, threshold2=thres2)
-# 이미지 처리 후 처리용 폴더 경로에(새로운) 처리한 이미지 저장해야 함
-
-# 이미지 색상 저장 csv file로 변환
-
-#이미지 사람과 옷 태두리
-
+# origin_img = cv2.imread(test_path)
+# copy_img=cv2.imread(test_path)
+# with mp_img_pose.Pose(
+# static_image_mode=True,
+# enable_segmentation=True,
+# min_detection_confidence=0.5) as pose:
+#     test_img = cv2.cvtColor(origin_img, cv2.COLOR_RGB2BGR)
+#
+#     img_pose = pose.process(test_img)
+#
+#     landmarks = img_pose.pose_landmarks.landmark  #각 랜드마크의 활성화 상태 리스트
+#
+#     origin_img_height, origin_img_width, _ = origin_img.shape
+#     img_shape=[origin_img_width, origin_img_height]
+#
+#     left_shoulder = (landmarks[mp_img_pose.PoseLandmark.LEFT_SHOULDER].x ,
+#                      landmarks[mp_img_pose.PoseLandmark.LEFT_SHOULDER].y )
+#     right_shoulder=(landmarks[mp_img_pose.PoseLandmark.RIGHT_SHOULDER].x,
+#                     landmarks[mp_img_pose.PoseLandmark.RIGHT_SHOULDER].y )
+#
+#     left_mouth=(landmarks[mp_img_pose.PoseLandmark.MOUTH_LEFT.value].x,
+#                 landmarks[mp_img_pose.PoseLandmark.MOUTH_LEFT.value].y)
+#     right_mouth=(landmarks[mp_img_pose.PoseLandmark.MOUTH_RIGHT.value].x,
+#                  landmarks[mp_img_pose.PoseLandmark.MOUTH_RIGHT.value].y)
+#
+#     left_pinky=(landmarks[mp_img_pose.PoseLandmark.LEFT_PINKY.value].x,
+#                 landmarks[mp_img_pose.PoseLandmark.LEFT_PINKY.value].y)
+#
+#     shoulder = {'l':np.multiply(left_shoulder, img_shape).astype(int),'r':np.multiply(right_shoulder,img_shape).astype(int)}
+#     mouth = {'l':np.multiply(left_mouth , img_shape).astype(int),'r': np.multiply(right_mouth, img_shape).astype(int)}
+#     right_elbow_x = np.multiply(landmarks[mp_img_pose.PoseLandmark.RIGHT_ELBOW].x, img_shape[0]).astype(int)
+#     left_pinky = np.multiply(left_pinky, img_shape).astype(int)
+#
+#     middle_neck_y= (((shoulder['l'][1]+shoulder['r'][1])//2)+((mouth['l'][1] + mouth['r'][1]) // 2))//2
+#
+#     reset_test_img = cv2.cvtColor(test_img, cv2.cv2.COLOR_BGR2RGB)
+#
+#     mp_pose_img_drawing.draw_landmarks(reset_test_img, img_pose.pose_landmarks, mp_img_pose.POSE_CONNECTIONS,
+#                                        mp_pose_img_drawing.DrawingSpec(color=(245, 117, 66), thickness=2,
+#                                                                        circle_radius=2),
+#                                        mp_pose_img_drawing.DrawingSpec(color=(245, 66, 230), thickness=2,
+#                                                                        circle_radius=2))
+#     #cv2.circle(reset_test_img, (right_elbow_x, middle_neck_y), radius=5, color=(225, 130, 130), thickness=1) - fin
+#
+# roi = origin_img[middle_neck_y:left_pinky[1], right_elbow_x :left_pinky[0]]
+# glay_img = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+# gaussian_blur =cv2.GaussianBlur(glay_img ,ksize=(3,3),sigmaX=0)
+#
+# thresh_img= np.median(gaussian_blur)
+# thresh_lower=int(max(0,(1.0 - 0.22) *thresh_img))
+# thresh_upper=int(max(255, (1.0 + 0.22)*thresh_img))
+#
+# #케니필터로
+# cv2.adaptiveThreshold(gaussian_blur, 255, cv2.ADAPTIVE_THRESH_MEAN_C , cv2.THRESH_BINARY_INV, 9 ,10)
+# edge=cv2.Canny(gaussian_blur, threshold1=thresh_lower, threshold2=thresh_upper)
+#
+# cv2.imshow(img_path+'1', edge)
+# cv2.imshow(img_path+'3', reset_test_img)
+#
+# cv2.imwrite(img_path+'1' + '.jpg', edge)
+# cv2.imwrite(img_path+'3' + '.jpg', reset_test_img)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
